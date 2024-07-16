a33b2ea5b049d3a4592d88d4b5b779551516096d
Dev-Abs <abdullah.devorbit@gmail.com>
2024-07-16 10:32:34 +0500

    UseRef component created, and explanation provided

---
+++ hooks/src/components/UseRef.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const UseRef = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UseRef
+
+
+// useRef Hook, explain
+// useRef Hook is used to create mutable references that persist for the entire lifetime of the component.
+// It is a function that returns a mutable object with a current property that can be used to store values.
+// The value of the current property can be changed without triggering a re-render of the component.
+// useRef is commonly used to access DOM elements, store previous values, and create timers or intervals.
+// It is similar to the ref attribute in class components, but can be used in functional components.
+// The useRef Hook is a close replacement for the ref attribute in class components.
+
