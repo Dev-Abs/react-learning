2d6c9cb8335f41bf911b0d1c130db68787a85b4a
Dev-Abs <abdullah.devorbit@gmail.com>
2024-07-16 11:06:15 +0500

    useRef Hook completed

--- hooks/src/components/UseRef.jsx
+++ hooks/src/components/UseRef.jsx
@@ -2,29 +2,40 @@ import React, {useEffect, useRef, useState} from 'react'
 
 const UseRef = () => {
     // const [count, setCount] = useState(0);
-    const [value, setValue] = useState(0);
-
-
-
+    // const [value, setValue] = useState(0);
     // useEffect(() => {
     //     setCount(prev => prev + 1);
     // });   // when using no dependency array, it will run on every render, it is increasing count infinitely, why?, because it is updating the state and causing a re-render, which triggers the useEffect again, and so on.
 
     // so we will use useRef to prevent this infinite loop, how?, useRef is used to store values that persist for the entire lifetime of the component, and it does not trigger a re-render when the value changes.
 
-    const count = useRef(0);
+    // const count = useRef(0);
     // useRef returns an object with a current property
 
-    useEffect(() => {
-        count.current += 1;
-    });
+    // useEffect(() => {
+    //     count.current += 1;
+    // });
+
+    // useRef for accessing DOM elements
+
+    const inputRef = useRef(null);
+
+    const btnClicked = () => {
+        console.log(inputRef.current.value);
+        inputRef.current.focus();
+        inputRef.current.style.background = 'red';
+    }
+
   return (
     <h1>
-    <button onClick={()=>{setValue(prev => prev - 1)}}>-1</button>
+    {/* <button onClick={()=>{setValue(prev => prev - 1)}}>-1</button>
     <h2>{value}</h2>
     <button onClick={()=>{setValue(prev => prev +1)}}>+1</button>
-    {/* <h2>Render Count: {count}</h2> */}
-    <h2>Render Count: {count.current}</h2>
+    {/* <h2>Render Count: {count}</h2> */} 
+    {/* <h2>Render Count: {count.current}</h2> */}
+
+    <input type="text" ref={inputRef} />
+    <button onClick={btnClicked} >Click Here</button>
     </h1>
   )
 }
