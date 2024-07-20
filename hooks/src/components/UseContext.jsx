0cc82b59858a470a388237cb20d2bfaca88ce604
Dev-Abs <abdullah.devorbit@gmail.com>
2024-07-20 20:15:45 +0500

    UseContext component created, and explanation provided

---
+++ hooks/src/components/UseContext.jsx
@@ -0,0 +1,14 @@
+// UseContext Hook, explain? , useContext is a hook that allows you to subscribe to React context without introducing nesting.
+// useContext(MyContext) only lets you read the context and subscribe to its changes. You still need a <MyContext.Provider> above in the tree to provide the value for this context.
+// useContext(MyContext) is equivalent to static contextType = MyContext in a class, or <MyContext.Consumer>.
+import React from 'react'
+
+const UseContext = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UseContext
