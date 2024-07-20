f94c58abf5dc4630145d58cfcb90d0e83d7b4c7b
Dev-Abs <abdullah.devorbit@gmail.com>
2024-07-20 20:19:43 +0500

    steps provided to use useContext hook

--- hooks/src/components/UseContext.jsx
+++ hooks/src/components/UseContext.jsx
@@ -1,6 +1,14 @@
 // UseContext Hook, explain? , useContext is a hook that allows you to subscribe to React context without introducing nesting.
 // useContext(MyContext) only lets you read the context and subscribe to its changes. You still need a <MyContext.Provider> above in the tree to provide the value for this context.
 // useContext(MyContext) is equivalent to static contextType = MyContext in a class, or <MyContext.Consumer>.
+// Use context hook allows you to access data from any level of the component tree without having to pass down props to each level.
+// steps to use useContext hook
+// 1. Create a context using React.createContext() method.
+// 2. Wrap the parent component inside the context provider.
+// 3. Use useContext hook to access the data from the context.
+// 4. Pass the context value to the provider.
+// 5. Use the context value in the child component.
+
 import React from 'react'
 
 const UseContext = () => {
